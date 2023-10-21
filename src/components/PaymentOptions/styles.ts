d7e6cffae33b786881441b3a02f6e63447720d22
1carlosrenan <carlosrenan96@gmail.com>
2023-10-21 08:38:14 -0300

    feat: PaymentOptions structure

--- src/components/PaymentOptions/styles.ts
+++ src/components/PaymentOptions/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div``
+
+export const Body = styled.div``
+
+export const Footer = styled.div``
+
+export const CardsList = styled.div``
+
+export const CardItem = styled.div``
+
+export const CardInfo = styled.div``
+
+export const AddCard = styled.div``
