e883f6599f99bf6a676196bfa8871475a5ba98ff
1carlosrenan <carlosrenan96@gmail.com>
2023-10-26 18:55:33 -0300

    feat: FormProfile generate

---
+++ src/components/FormProfile/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
+import FormProfile from '.'
+
+describe('<FormProfile />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<FormProfile />)
+
+    expect(
+      screen.getByRole('heading', { name: /FormProfile/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
